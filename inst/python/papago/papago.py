7ee581df7478a33bc2e1457ae76b1a51eed0502e
seokhoonj <seokhoonj@gmail.com>
2023-08-18 13:14:47 +0900

    `detect_language` function is added for the further development

--- inst/python/papago/papago.py
+++ inst/python/papago/papago.py
@@ -1,4 +1,6 @@
 # NAVER Papago Text Translation API
+import os
+import sys
 import urllib.request
 import json
 import numpy as np
@@ -116,6 +118,32 @@ class Translator:
                 })], axis = 1)
         return translated_data
 
+    def detect_language(self, text):
+        '''
+        Client ids and secrets of text translation and 
+        language detection are different.
+        '''
+        encQuery = urllib.parse.quote(text)
+        data = "query=" + encQuery
+        if self.platform:
+            url = "https://naveropenapi.apigw.ntruss.com/langs/v1/dect"
+            request = urllib.request.Request(url)
+            request.add_header("X-NCP-APIGW-API-KEY-ID", self.client_id)
+            request.add_header("X-NCP-APIGW-API-KEY", self.client_secret)
+        else:
+            url = "https://openapi.naver.com/v1/papago/detectLangs"
+            request = urllib.request.Request(url)
+            request.add_header("X-Naver-Client-Id", self.client_id)
+            request.add_header("X-Naver-Client-Secret", self.client_secret)
+        response = urllib.request.urlopen(request, data = data.encode("utf-8"))
+        rescode = response.getcode()
+        if (rescode == 200):
+            response_body = response.read()
+            detected_language = response_body.decode('utf-8')
+        else:
+            detected_language = "Error Code:" + rescode
+        return detected_language
+     
     def read_sheets(path):
         return [sht for sht in pd.read_excel(
             path, sheet_name = None, skiprows = 0, engine = 'openpyxl').keys()]
